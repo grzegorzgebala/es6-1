4d3d326317729da14d87e37a26ebb328f7bcaec2
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-12-13 00:16:55 +0100

    control commit

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-// Zad1
-const sayHello = (name = 'World', hello = 'Hello') => console.log(`${hello} ${name}!`);
-sayHello();
+// // Zad1
+// const sayHello = (name = 'World', hello = 'Hello') => console.log(`${hello} ${name}!`);
+// sayHello();
 
-// Zad 2
-let multiply = (x, y = '1') => {return x * y};
-console.log(multiply(2, 5));
-console.log(multiply(6, 6));
-console.log(multiply(5));
+// // Zad 2
+// let multiply = (x, y = '1') => {return x * y};
+// console.log(multiply(2, 5));
+// console.log(multiply(6, 6));
+// console.log(multiply(5));
 
 // Zad 3
-const average = (...args) => args.forEach(arg => console.log((arg += 1)/arg));
+let sum;
+let average = (...args) => args.forEach(arg => {
+	arg += arg;
+	sum = arg;
+	return console.log(sum);
+});
 
-average(1);
-average(1, 3);
+// average(1);
+// average(1, 3);
 average(1, 3, 6, 6);
 
-// Zad 4
+// // Zad 4
 
-const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+// const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 
 
-let averages = (...args) => grades.forEach(arg => console.log((arg += 1)/arg));
+// let averages = (...args) => grades.forEach(arg => console.log((arg += 1)/arg));
 
-averages();
+// averages();
